4d74292d07f515f27c4d14949661b71bdd1947b2
daniel.loback <daniel.loback@gmail.com>
2022-04-28 13:03:22 -0300

    Criação de rotina para erro, rota com informações da API, GET e POST de competição

--- COB.py
+++ COB.py
@@ -14,25 +14,54 @@ def Landpage():
 
 @app.route('/competicao', methods = ['GET', 'POST'])
 def competicao():
-    if(request.method == 'GET'):
-        with get_mysql_conn() as conn:
-            cursor = conn.cursor(dictionary=True)
-            cursor.execute("select Id_Competicao, Nome_Competicao from Competicao")
+    with get_mysql_conn() as conn:
+        if(request.method == 'GET'):
+            cursor  = conn.cursor(dictionary=True)
+            cursor.execute("select * from Competicao")
             results = cursor.fetchall()
             return(jsonify(results))
 
+        if(request.method == 'POST'):
+            Nome_Competicao     = str(request.json['Nome_Competicao'])
+            Data_Inscricao      = str(request.json['Data_Inscricao'])
+            Metrica_Competicao  = str(request.json['Metrica_Competicao'])
+            cursor  = conn.cursor(dictionary=True)
+
+            cursor.execute("SELECT Id_Competicao From Competicao Where Nome_Competicao = '{0}'".format(Nome_Competicao))
+            valid = cursor.fetchone()
+
+            if not bool(valid):
+                insert_competicao = """Insert Into Competicao (Nome_Competicao, Data_Inscricao, Metrica_Competicao) values ('{0}','{1}','{2}')""".format(Nome_Competicao, Data_Inscricao, Metrica_Competicao)
+                print(insert_competicao)
+                cursor.execute(insert_competicao)
+                conn.commit()
+                cursor.execute("SELECT LAST_INSERT_ID() as Id_Competicao")
+                results = cursor.fetchone()
+                return(jsonify(results))
+            else:
+                return {"Err": "Competição já existe no banco de dados"}
 
 @app.route('/atleta', methods = ['GET', 'POST'])
 def atleta():
-    if(request.method == 'GET'):
-        with get_mysql_conn() as conn:
-            cursor = conn.cursor(dictionary=True)
+    with get_mysql_conn() as conn:
+        if(request.method == 'GET'):
+            cursor  = conn.cursor(dictionary=True)
             cursor.execute("select * from Atleta")
             results = cursor.fetchall()
             return(jsonify(results))
 
+@app.route('/api/v1', methods=["GET"])
+def info_view():
+    output = {
+        'info': 'GET /api/v1',
+        'competicao': 'GET /competicao',
+        'inserir competicao': 'POST /competicao',
+    }
+    return jsonify(output)
 
-@app.errorhandler(Exception)
+
+
+@app.errorhandler(HTTPException)
 def exception_handler(error):
     return(jsonify({"code": error.code, "name": error.name, "description": error.description,}))
 
