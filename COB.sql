4a10ededbd761d662f09b50b80cb6d572d501062
daniel.loback <daniel.loback@gmail.com>
2022-04-28 13:04:12 -0300

    Criação de rota com informações da API, rotina de erro, GET e POST de competição

--- COB.sql
+++ COB.sql
@@ -3,22 +3,10 @@
 -- -----------------------------------------------------
 DROP DATABASE IF EXISTS COB;
 
-CREATE DATABASE IF NOT EXISTS COB DEFAULT CHARACTER SET UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE DATABASE IF NOT EXISTS COB; -- DEFAULT CHARACTER SET UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
 
 USE COB;
-
--- -----------------------------------------------------
--- Desabilitando a opção default do mysql, pois no final do script faço 2 exclusões 
--- de registros que não existem na tabela de relacionamento
--- -----------------------------------------------------
 SET SQL_SAFE_UPDATES = 0;
-
--- -----------------------------------------------------
--- Optei por criar as tabelas sem as chaves estrangeiras
--- Com isso, consigo inserir os registros sem critica na ordem de execução, 
--- se fosse o caso de carga de dados
--- Posteriormente faço update das chaves estrangeiras nas devidas tabelas
--- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Criando tabela Competição
@@ -29,8 +17,6 @@ CREATE TABLE IF NOT EXISTS  COMPETICAO (
   Data_Inscricao date NOT NULL,
   Data_Fim date,
   Metrica_Competicao char(1) NOT NULL,
-  Id_Atleta INT,
-  Resultado_Campeao Float,
   PRIMARY KEY (Id_Competicao));
 
 -- -----------------------------------------------------
@@ -41,23 +27,9 @@ CREATE TABLE IF NOT EXISTS ATLETA (
   Nome_Atleta VARCHAR(255) NOT NULL,
   Id_Competicao INT NOT NULL,
   Resultado_Atleta Float NOT NULL,
-  PRIMARY KEY (Id_Atleta));
-
--- -----------------------------------------------------
--- Depois das tabelas criadas e populadas (caso ocorresse)
--- Crio os relacionamentos entre as chaves
--- -----------------------------------------------------
-
-ALTER TABLE COMPETICAO ADD
-  CONSTRAINT fk_Atleta_Competicao
-    FOREIGN KEY (Id_Atleta)
-    REFERENCES Atleta (Id_Atleta)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;    
-
-ALTER TABLE ATLETA ADD
+  PRIMARY KEY (Id_Atleta),
   CONSTRAINT fk_Competicao_Atleta
     FOREIGN KEY (Id_Competicao)
     REFERENCES COMPETICAO (Id_Competicao)
     ON DELETE CASCADE
-    ON UPDATE CASCADE;  
+    ON UPDATE CASCADE);  
